97dcdc91125d046e65f61b340f506b727313c4e3
Daniel Asena <asenadaniel@gmail.com>
2024-07-09 16:20:16 +0100

    .

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,17 +1,7 @@
-import React, { useContext } from 'react'
-import { ProductContext } from '../../Context/productContext';
+import React from 'react'
+
 
 function ProductCard({ name, img, price }) {
-
-  const { addToCart } = useContext(ProductContext);
-
-  const handleAddToCart = () => {
-    const product = { name, img, price };
-    addToCart(product, 1);
-    console.log(product)
-  };
-
-
   return (
     <div className=' '>
       <div className=' flex flex-col justify-center items-center  '>
@@ -20,7 +10,7 @@ function ProductCard({ name, img, price }) {
           <p className=' font-bold'>{name}</p>
           <p>{price}NGN</p>
         </div>
-        <button className=' bg-green-900 w-[80%] h-[35px] xl:h-[53px] text-[13px] xl:text-[17px] rounded-b-xl  text-white' onClick={handleAddToCart}>ADD TO CART</button>
+        <button className=' bg-green-900 w-[80%] h-[35px] xl:h-[53px] text-[13px] xl:text-[17px] rounded-b-xl  text-white'>ADD TO CART</button>
       </div>
     </div>
   )
